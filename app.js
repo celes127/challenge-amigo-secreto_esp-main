ade7288e25113173487ca8ca8461734396dfe66d
Celeste Gutierrez <celesgutierrez@frba.utn.edu.ar>
2025-09-17 16:25:23 -0300

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Variables
+let lista = [];
+
+function agregarAmigo() 
+{
+    
+    let nombre = document.getElementById('amigo').value;
+    
+    if (nombre != '')
+    {
+        lista.push(nombre);
+        vaciarCaja();
+        recorrerArray();
+    }
+    else
+    {
+        alert('Ingresar un nombre valido');
+    }
+    console.log(lista);
+    return;
+}
+
+function vaciarCaja()
+{
+    let valorCaja = document.querySelector('#amigo');
+    valorCaja.value = '';
+    return;
+}
+
+function recorrerArray ()
+{
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
+    lista.forEach((item) => 
+            {
+                let listItem = document.createElement("li");
+                listItem.textContent = item;
+                listaAmigos.appendChild(listItem);
+            });
+    return;
+}
